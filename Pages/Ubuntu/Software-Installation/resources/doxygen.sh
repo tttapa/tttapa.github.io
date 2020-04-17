17c047ebb777da4fd4a8084f925157de2fde2e6a
Pieter Pas <pieter.pas@outlook.com>
2020-04-17 19:51:08 +0200

    Update doxygen version to 1.8.18

--- Pages/Ubuntu/Software-Installation/resources/doxygen.sh
+++ Pages/Ubuntu/Software-Installation/resources/doxygen.sh
@@ -3,7 +3,7 @@
 set -ex
 
 cd /tmp
-git clone --branch Release_1_8_16 --depth 1 https://github.com/doxygen/doxygen.git
+git clone --branch Release_1_8_18 --depth 1 https://github.com/doxygen/doxygen.git
 mkdir doxygen/build && cd doxygen/build
 cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
 make -j`nproc`
